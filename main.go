59dcffeb53223a3dd0b2c800706bb37ac326916c
liukun <liukunauh@163.com>
2024-11-08 01:06:46 +0800

    feat: define input

--- main.go
+++ main.go
@@ -15,37 +15,42 @@ func main() {
 		}
 	}()
 	app := &cli.App{
-		Name:  "thrift_format",
-		Usage: "format thrift file",
-		Action: func(c *cli.Context) error {
-			return nil
-		},
-		Version: "0.1.0",
+		Name:    "thrift_format",
+		Usage:   "format thrift file",
+		Action:  action,
+		Version: "0.0.1",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:     "file",
 				Aliases:  []string{"f"},
 				Usage:    "`file` to format",
-				Required: false,
+				Required: true,
 			},
 			&cli.BoolFlag{
 				Name:        "overwrite",
 				Aliases:     []string{"o"},
 				DefaultText: "true",
-				Usage:       "overwrite file or not, default true",
+				Usage:       "overwrite file or not",
 				Required:    false,
 				Value:       true,
 			},
-			&cli.BoolFlag{
-				Name:        "verbose",
-				DefaultText: "false",
-				Usage:       "display debug log",
-				Required:    false,
-				Value:       true,
+			&cli.StringFlag{
+				Name:     "lines",
+				Aliases:  []string{"l"},
+				Usage:    "`lines` selected to format",
+				Required: false,
 			},
 		},
 	}
 	if err := app.Run(os.Args); err != nil {
 		color.Red("%v", err)
 	}
+}
+
+func action(ctx *cli.Context) error {
+	filePath := ctx.String("file")
+	color.Green("file path: %s", filePath)
+	lines := ctx.String("lines")
+	color.Green("lines: %s", lines)
+	return nil
 }
