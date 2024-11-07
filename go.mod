355c49b518cef5cf6c24f18fa1dd3879bf09af31
liukun <liukunauh@163.com>
2024-11-08 00:55:36 +0800

    feat: define input

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module gitee.com/liukunc9/thrift_format
 
 go 1.21
+
+require github.com/urfave/cli/v2 v2.27.5
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"github.com/fatih/color"
+	"github.com/urfave/cli/v2"
+	"os"
+	"runtime/debug"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	defer func() {
+		if r := recover(); r != nil {
+			color.Red("panic: %v", r)
+			color.Red("%s", string(debug.Stack()))
+		}
+	}()
+	app := &cli.App{
+		Name:  "thrift_format",
+		Usage: "format thrift file",
+		Action: func(c *cli.Context) error {
+			return nil
+		},
+		Version: "0.1.0",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "file",
+				Aliases:  []string{"f"},
+				Usage:    "`file` to format",
+				Required: false,
+			},
+			&cli.BoolFlag{
+				Name:        "overwrite",
+				Aliases:     []string{"o"},
+				DefaultText: "true",
+				Usage:       "overwrite file or not, default true",
+				Required:    false,
+				Value:       true,
+			},
+			&cli.BoolFlag{
+				Name:        "verbose",
+				DefaultText: "false",
+				Usage:       "display debug log",
+				Required:    false,
+				Value:       true,
+			},
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		color.Red("%v", err)
+	}
 }
