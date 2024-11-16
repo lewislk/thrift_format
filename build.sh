8d99b3d722c9ea30045baecd4316de9a31c67248
liukun <liukunauh@163.com>
2024-11-16 12:00:08 +0800

    feat: build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 RUN_NAME="thrift_format"
 
-# windows
-go build -o output/bin/${RUN_NAME}.exe
+# go通过使用不同的环境变量可以打包不同平台运行的程序
 
-# linux/mac os
-#go build -o output/bin/${RUN_NAME}
+# mac下的环境变量
+# intel cpu
+CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o output/bin/${RUN_NAME}-mac-amd64
+# apple cpu
+CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -o output/bin/${RUN_NAME}-mac-arm64
+
+# linux的环境变量
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o output/bin/${RUN_NAME}-linux-amd64
+
+# windows的环境变量
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o output/bin/${RUN_NAME}.exe
